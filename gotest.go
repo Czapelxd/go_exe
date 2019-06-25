ee3a1684ff05d43ab4e3531f3059a80e9aa5c283
Michal Czaplicki <mc222667@falmouth.ac.uk>
2019-06-25 20:25:52 +0100

    converting varables

--- gotest.go
+++ gotest.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-//  "/" slash give us access to package
+// the func paramiter must be specify and the result too
+
+func add(x, y float64) float64 {
+	return x + y
+}
+
+const x int = 5
+
+func multiple(a, b string) (string, string) {
+	return a, b
+}
 
 func main() {
-	fmt.Println("A number from 1-100", rand.Intn(100))
+	// num1, num2 := 5.6, 9.5
+
+	// above shortcut works only 64 byte precision
+	// in case of using 32 the var hat to look like:
+	// var num1, num2 float32 = 5.6, 9.5
+
+	// fmt.Println(add(num1, num2))
+
+	w1, w2 := "Hey", "there"
+
+	fmt.Println(multiple(w1, w2))
+
 }
